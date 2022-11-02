a79f085b176b07d9e865b40a0249e3d83ea78565
xiuxiuyifan <1575243732@qq.com>
2022-11-02 22:37:05 +0800

    编写一个脚本自动生成readme

--- readme.md
+++ readme.md
@@ -1,1 +1,45 @@
 ## 前端手写题积累
+
+[ 01数组扁平化 ](01数组扁平化.js)
+
+[ 02实现一个getType获取数据类型 ](02实现一个getType获取数据类型.js)
+
+[ 03new一个对象的过程 ](03new一个对象的过程.js)
+
+[ 04深度遍历一个DOM树 ](04深度遍历一个DOM树.html)
+
+[ 05广度优先遍历一个DOM树 ](05广度优先遍历一个DOM树.html)
+
+[ 06深度遍历DOM树，不使用递归 ](06深度遍历DOM树，不使用递归.html)
+
+[ 07lzayMan实现sleep机制 ](07lzayMan实现sleep机制.js)
+
+[ 08实现一个curry函数实现函数柯里化 ](08实现一个curry函数实现函数柯里化.js)
+
+[ 09实现instanceof ](09实现instanceof.js)
+
+[ 10实现bind函数 ](10实现bind函数.js)
+
+[ 11实现call函数 ](11实现call函数.js)
+
+[ 12实现apply ](12实现apply.js)
+
+[ 13实现eventbus ](13实现eventbus.js)
+
+[ 14实现EventBus-on-once分开存贮 ](14实现EventBus-on-once分开存贮.js)
+
+[ 15js实现一个LRU缓存 ](15js实现一个LRU缓存.js)
+
+[ 16不用Map实现一个LRU缓存 ](16不用Map实现一个LRU缓存.js)
+
+[ 17实现一个深拷贝 ](17实现一个深拷贝.js)
+
+[ 18手写promise ](18手写promise.js)
+
+[ 19迭代器 ](19迭代器.js)
+
+[ 20generate实现async和await ](20generate实现async和await.js)
+
+[ 21promise面试题 ](21promise面试题.js)
+
+[ 22控制并发 ](22控制并发.js)
--- scripts/build.js
+++ scripts/build.js
@@ -2,5 +2,52 @@
 // 遍历当前目录下面的文件  [.js|.html]结尾的文件生成链接写入到文件当中去
 
 
-const fs = require('fs')
 const path = require('path')
+const fs = require('fs/promises')
+
+const flag = `## 前端手写题积累`
+
+const ignoreFiles = ['index.html']
+
+// 获取源文件内容
+async function getSourceContent() {
+  let content = await fs.readFile(path.resolve(__dirname, '../readme.md'), { encoding: 'utf-8' })
+  return content
+}
+// 生成新的字符串
+
+async function genNewReadmeStr() {
+  let files = await fs.readdir(path.resolve(__dirname, '../'))
+  let extensions = ['.js', '.html']
+  files = files.filter((name) => {
+    const extension = path.extname(name)
+    return extensions.includes(extension) && !ignoreFiles.includes(name)
+  })
+  const newStr = files.map((name) => {
+    // 去掉后缀只拿名字
+    const lastIndex = name.lastIndexOf('.')
+    const filename = name.substring(0, lastIndex)
+    return `[ ${filename} ](${name})`
+  }).join('\n\n')
+  return newStr
+}
+
+// 拼接后写入文件
+
+
+async function start() {
+  let sourceContent = await getSourceContent()
+  sourceContent = sourceContent.substring(0, flag.length)
+  let newContent = await genNewReadmeStr()
+
+  let finallyContent = `${sourceContent ? sourceContent : flag}\n\n${newContent}`
+
+  await fs.writeFile(path.resolve(__dirname, '../readme.md'), finallyContent, {
+    encoding: 'utf-8'
+  })
+  console.log(`readme文件生成完毕`)
+}
+
+start()
+
+
